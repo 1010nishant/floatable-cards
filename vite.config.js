f885ed347227a6d5cd33494e34eb4d5c5eae5eae
1010nishant <nishantjangid6377@gmail.com>
2024-03-28 15:34:44 +0530

    vite edited

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
     base: '/',
   }
 
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
+  // if (command !== 'serve') {
+  //   config.base = '/react-vite-gh-pages/'
+  // }
 
   return config
 })
