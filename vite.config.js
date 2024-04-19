fc52f8db0f34577fc48cf03f4a5f9b88b1394bbb
1010nishant <nishantjangid6377@gmail.com>
2024-04-19 19:30:37 +0530

    github page deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/floatable-cards/',
   plugins: [react()],
   server: {
     host: true
