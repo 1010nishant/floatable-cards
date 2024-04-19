5a698a18b1d3806db2fc0e2095e355d1ae59fd81
1010nishant <nishantjangid6377@gmail.com>
2024-04-19 19:58:08 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
+export default defineConfig({
+  base: '/floatable-cards/',
+  plugins: [react()],
+  server: {
+    host: true
   }
-
-  if (command !== 'serve') {
-    config.base = '/floatable-cards/'
-  }
-
-  return config
 })
